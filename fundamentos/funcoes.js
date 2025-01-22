5dfdc27310fdfbf6e867b447128c4f221396718c
Yesenia Salas <Yesisalas1804@gmail.com>
2025-01-22 17:14:14 -0300

    Estudos de JavaScript

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -6,4 +6,45 @@
 function hello() {
     console.log("Hello function")
 }
+console.log(typeof(hello))
 hello()
+
+// Função atribuída
+const hello2 = function (){
+    console.log("Hello function assigned")
+}
+console.log(typeof(hello2))
+hello2()
+
+// Arrow function =>(simplificação da função atribuida)
+const hello3 = () => {
+    console.log("Hello arrow function")
+}
+
+console.log(typeof(hello3))
+hello3()
+
+// Funções com passagem de parâmetros e retorno
+//  Função literal
+function somarS(num1, num2) {
+    return (console.log(num1+num2))
+}
+somarS(2, 3)
+
+//função atribuida
+const somarA = function(num1, num2) {
+ 
+    return (console.log(num1+num2))
+}
+somarA(4, 5)
+// arrow function
+const somarAF = function(num1, num2) {
+ 
+    return (console.log(num1+num2))
+}
+somarAF(4, 5)
+
+//arrow function simplificada (retorno e implicito)
+const somarAFS = (num1, num2) => console.log(num1+num2)
+
+somarAFS(2, 2)
---
+++ fundamentos/operadores.js
@@ -0,0 +1,31 @@
+/**
+ * OPERADORES
+ * - Aritiméticos
+ * - Aritiméticos de atribuição
+ * - Comparação 
+ * - Lógicos
+ */
+
+console.clear()
+console.log("Operadores aritiméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (resto da divisão")
+console.log("")
+console.log("Operadores aritiméticos de atribuição")
+console.log("+= | soma e atribuição a variável")
+console.log("-= | subtração e atribuição a variável")
+console.log("*= | multiplicação e atribuição a variável")
+console.log("/= | divisão e atribuição a variável")
+console.log(" ++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 ao resultado")
+console.log("")
+console.log("Comparadores")
+console.log(" > | maior que ")
+console.log(" < | menor que ")
+console.log(" >= | maior ou igual a ")
+console.log(" <= | menorou iguial a ")
+console.log(" == | idêntico a")
+console.log(" !== | não idêntico a")
